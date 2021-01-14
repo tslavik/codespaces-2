367810f929972d4c646a26bc1ea269613df0dfaa
Tomas Slavik <tomas.slavik@microsoft.com>
2021-01-14 13:58:18 +0000

    receiver fixed in 7.0.2

--- ReceiveMessages/index.ts
+++ ReceiveMessages/index.ts
@@ -3,6 +3,8 @@ import moment from "moment";
 import {AzureFunction, Context, HttpRequest} from "@azure/functions"
 
 const _start = moment();
+const queueName = "test";
+let _messages = 0;
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void>  {
     context.log('Start Receiving');
@@ -10,9 +12,9 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
     const connectionString = process.env.SB_CONN_STR || "<connection string>";
     const sbClient = new ServiceBusClient(connectionString);
     const sessionId = "session-1";
-    const queueName = "test";
+    // const queueName = "test";
     
-    await receiveMsg(sbClient,sessionId,queueName)
+    await receiveMessages(40000)
     context.res = {
       status: 200
       };
@@ -20,69 +22,126 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
     
 };
 
-async function receiveMsg(sbClient: ServiceBusClient, sessionId: string, queueName: string) {
+async function receiveMessages(allmsg: number) {
+  
+  const maxConcurrentCalls = 100;
+  const allMessages = allmsg;
 
-  // - If receiving from a subscription you can use the createReceiver(topicName, subscriptionName) overload
-  // instead.
-  // - See session.ts for how to receive using sessions.
-  // const receiver = sbClient.createReceiver(queueName);
+  const writeResultsPromise = WriteResults(allMessages);
+  await RunTest("session-1",maxConcurrentCalls, allMessages);
+  await writeResultsPromise;
+}
+
+async function  RunTest(
+  sessionId: string,
+  maxConcurrentCalls: number,
+  messages: number
+): Promise<void> {
+  const connectionString = process.env.SB_CONN_STR || "<connection string>";
+  const sbClientLocal = new ServiceBusClient(connectionString);
   const options:ServiceBusSessionReceiverOptions = {receiveMode:"receiveAndDelete"};
-  const receiver = await sbClient.acceptSession(queueName, sessionId, options);
-  const subscribeOptions:SubscribeOptions = {maxConcurrentCalls:100,autoCompleteMessages:false};
-  let msgc = 0;
-  try {
-    const subscription = receiver.subscribe({
-      // After executing this callback you provide, the receiver will remove the message from the queue if you
-      // have not already settled the message in your callback.
-      // You can disable this by passing `false` to the `autoCompleteMessages` option in the `subscribe()` method.
-      // If your callback _does_ throw an error before the message is settled, then it will be abandoned.
-      
-      processMessage: async (brokeredMessage: ServiceBusReceivedMessage) => {
-        msgc++;
-        console.log(`${msgc}`);
-      },
-      
-      // This callback will be called for any error that occurs when either in the receiver when receiving the message
-      // or when executing your `processMessage` callback or when the receiver automatically completes or abandons the message.
-      processError: async (args: ProcessErrorArgs) => {
-        console.log(`Error from source ${args.errorSource} occurred: `, args.error);
+  const receiver = await sbClientLocal.acceptSession(queueName, sessionId, options);
+  const subscribeOptions:SubscribeOptions = {maxConcurrentCalls:maxConcurrentCalls,autoCompleteMessages:false};
 
-        // the `subscribe() call will not stop trying to receive messages without explicit intervention from you.
-        if (isServiceBusError(args.error)) {
-          switch (args.error.code) {
-            case "MessagingEntityDisabled":
-            case "MessagingEntityNotFound":
-            case "UnauthorizedAccess":
-              // It's possible you have a temporary infrastructure change (for instance, the entity being
-              // temporarily disabled). The handler will continue to retry if `close()` is not called on the subscription - it is completely up to you
-              // what is considered fatal for your program.
-              console.log(
-                `An unrecoverable error occurred. Stopping processing. ${args.error.code}`,
-                args.error
-              );
-              await subscription.close();
-              break;
-            case "MessageLockLost":
-              console.log(`Message lock lost for message`, args.error);
-              break;
-            case "ServiceBusy":
-              // choosing an arbitrary amount of time to wait.
-              await delay(1000);
-              break;
+   const subscription = receiver.subscribe({
+        // After executing this callback you provide, the receiver will remove the message from the queue if you
+        // have not already settled the message in your callback.
+        // You can disable this by passing `false` to the `autoCompleteMessages` option in the `subscribe()` method.
+        // If your callback _does_ throw an error before the message is settled, then it will be abandoned.
+        
+        processMessage: async (brokeredMessage: ServiceBusReceivedMessage) => {
+          _messages++;
+          if (_messages === messages){
+            await receiver.close();
+            await sbClientLocal.close();
+          }
+          
+        },
+        
+        // This callback will be called for any error that occurs when either in the receiver when receiving the message
+        // or when executing your `processMessage` callback or when the receiver automatically completes or abandons the message.
+        processError: async (args: ProcessErrorArgs) => {
+          console.log(`Error from source ${args.errorSource} occurred: `, args.error);
+  
+          // the `subscribe() call will not stop trying to receive messages without explicit intervention from you.
+          if (isServiceBusError(args.error)) {
+            switch (args.error.code) {
+              case "MessagingEntityDisabled":
+              case "MessagingEntityNotFound":
+              case "UnauthorizedAccess":
+                // It's possible you have a temporary infrastructure change (for instance, the entity being
+                // what is considered fatal for your program.
+                console.log(
+                  `An unrecoverable error occurred. Stopping processing. ${args.error.code}`,
+                  args.error
+                );
+                await subscription.close();
+                break;
+              case "MessageLockLost":
+                console.log(`Message lock lost for message`, args.error);
+                break;
+              case "ServiceBusy":
+                // choosing an arbitrary amount of time to wait.
+                await delay(1000);
+                break;
+            }
           }
         }
-      }
-    },subscribeOptions);
-
-    // Waiting long enough before closing the receiver to receive messages
-    console.log(`Receiving messages for 60 seconds before exiting...`);
-    await delay(60000);
-
-    console.log(`Closing...`);
-    await receiver.close();
-  } finally {
-    await sbClient.close();
-  }
+      },subscribeOptions);
 }
+
+async function WriteResults(messages: number): Promise<void> {
+  let lastMessages = 0;
+  let lastElapsed = 0;
+  let maxMessages = 0;
+  let maxElapsed = Number.MAX_SAFE_INTEGER;
+
+  do {
+    await delay(1000);
+
+    const receivedMessages = _messages;
+    const currentMessages = receivedMessages - lastMessages;
+    lastMessages = receivedMessages;
+
+    const elapsed = moment().diff(_start);
+    const currentElapsed = elapsed - lastElapsed;
+    lastElapsed = elapsed;
+
+    if (currentMessages / currentElapsed > maxMessages / maxElapsed) {
+      maxMessages = currentMessages;
+      maxElapsed = currentElapsed;
+    }
+
+    WriteResult(
+      receivedMessages,
+      elapsed,
+      currentMessages,
+      currentElapsed,
+      maxMessages,
+      maxElapsed
+    );
+  } while (_messages < messages);
+}
+
+function WriteResult(
+  totalMessages: number,
+  totalElapsed: number,
+  currentMessages: number,
+  currentElapsed: number,
+  maxMessages: number,
+  maxElapsed: number
+): void {
+  Log(
+    `\tTot Msg\t${totalMessages}` +
+      `\tCur MPS\t${Math.round((currentMessages * 1000) / currentElapsed)}` +
+      `\tAvg MPS\t${Math.round((totalMessages * 1000) / totalElapsed)}` +
+      `\tMax MPS\t${Math.round((maxMessages * 1000) / maxElapsed)}`
+  );
+}
+
+function Log(message: string): void {
+  console.log(`[${moment().format("hh:mm:ss.SSS")}] ${message}`);
+}
+
 
 export default httpTrigger;
